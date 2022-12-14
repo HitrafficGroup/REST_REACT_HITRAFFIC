1ea2c77d0e25fd4ede4f6727ace45946b35d6a8e
david <https://github.com/HitrafficGroup/API_PYTHON_HITRAFFIC.git>
2022-12-14 10:43:11 -0500

    se agrego un indicador tipo switch al la vista home

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useMemo, useCallback, useRef, useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 import { collection, updateDoc, onSnapshot, doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase-config";
@@ -611,7 +614,10 @@ export default function HomeView() {
                                             {dato.mac}
                                         </Td>
                                         <Td >
-                                            {dato.status}
+                                           <FormControlLabel
+                                                control={<IOSSwitch sx={{ m: 1 }} defaultChecked estado={dato.status === 'online'? true:false} />}
+                                                    label={dato.status}
+                                                />
                                         </Td>
 
                                     </Tr>
@@ -851,6 +857,58 @@ export default function HomeView() {
     );
 
 }
+
+const IOSSwitch = styled((props) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} checked={props.estado}  />
+  ))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+      padding: 0,
+      margin: 2,
+      transitionDuration: '300ms',
+      '&.Mui-checked': {
+        transform: 'translateX(16px)',
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+          opacity: 1,
+          border: 0,
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+          opacity: 0.5,
+        },
+      },
+      '&.Mui-focusVisible .MuiSwitch-thumb': {
+        color: '#33cf4d',
+        border: '6px solid #fff',
+      },
+      '&.Mui-disabled .MuiSwitch-thumb': {
+        color:
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[600],
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxSizing: 'border-box',
+      width: 22,
+      height: 22,
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: 26 / 2,
+      backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+      opacity: 1,
+      transition: theme.transitions.create(['background-color'], {
+        duration: 500,
+      }),
+    },
+  }));
+
 
 
 const semaforo = new L.Icon({
